205eb6fa3f9bc7684d599cc90247d8fd69e1af98
sungyuha <yuha822@naver.com>
2023-12-02 14:35:38 +0900

    dosc: JS - 알고리즘, 문제 해결

--- ex/JS/Add.js
+++ ex/JS/Add.js
@@ -31,7 +31,9 @@ charCount('Your PIN number is 1234')
 }*/
 
 function charCount(str) {
-    // 주석은 가장 간단하게 무언가를 한다고 추가하고 이어서 출력값이 문자열 내의 소문자 영숫자 문자인 키를 지닌 객체를 반환해줌
+    // 주석은 가장 간단하게 무언가를 한다고 추가하고
+    //이어서 출력값이 문자열 내의 소문자 영숫자 문자인 키를 지닌 객체를 반환해줌
+    
 }
 
 function charCount(str) {
@@ -41,6 +43,24 @@ function charCount(str) {
         // char(문자)가 숫자/문자이며 객체에 없다면 추가하고 값을 1로 설정함
         // 이어서 문자가 공백, 마침표 등과 같은 다른 것이라면 아무것도 하지 않도록 해줌
     // 다음에 마지막에 객체를 반환
+}
 
 
+function charCount(str) {
+    // 마지막에 반환할 객체에 이어 문자열에 
+    let result = {};
+    // 루프를 적용하고 세부적인 부분을 작성함
+    for(let i = 0; i <str.length; i++) {
+        let char = str[i]
+        // char(문자)가 숫자/문자이며 객체에 키라면 1을 개수에 더하도록 함
+        if(result[char] > 0) {
+            result[char]++;
+        } else {
+            // char(문자)가 숫자/문자이며 객체에 없다면 추가하고 값을 1로 설정함
+            result[char] = 1;
+        };
+    }
+        return result;
+        // 이어서 문자가 공백, 마침표 등과 같은 다른 것이라면 아무것도 하지 않도록 해줌
+    // 다음에 마지막에 객체를 반환
 }
