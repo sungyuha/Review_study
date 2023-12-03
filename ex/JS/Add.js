3c386a3eca37639650bee63182cf3cecd5829a94
sungyuha <yuha822@naver.com>
2023-12-03 23:31:51 +0900

    dosc: JS - 정규식

--- ex/JS/Add.js
+++ ex/JS/Add.js
@@ -50,8 +50,8 @@ function charCount(str) {
     // 마지막에 반환할 객체에 이어 문자열에 
     let result = {};
     // 루프를 적용하고 세부적인 부분을 작성함
-    for(let i = 0; i <str.length; i++) {
-        let char = str[i]
+    for(let i = 0; i < str.length; i++) {
+        let char = str[i].toLowerCase(); // 문자열을 소문자로 변환해서 반환
         // char(문자)가 숫자/문자이며 객체에 키라면 1을 개수에 더하도록 함
         if(result[char] > 0) {
             result[char]++;
@@ -63,4 +63,20 @@ function charCount(str) {
         return result;
         // 이어서 문자가 공백, 마침표 등과 같은 다른 것이라면 아무것도 하지 않도록 해줌
     // 다음에 마지막에 객체를 반환
+}
+
+// 정규식
+function charCount(str) {
+    let obj = {};
+    for (let char of str) {
+        char = str[i].toLowerCase(); // for문에서 해당 문자열에 대해 let char를 적용하면 char를 다시 정의할 필요가 없음
+        if(/[a-z0-9]/.test(char)) {
+            if(obj[char] > 0) {
+                obj[char]++;
+            } else {
+                obj[char] = 1;
+            };
+        }
+    }
+    return obj;
 }
