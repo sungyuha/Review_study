2d9bf7ce7f40682916ac20fa64b30da0c777c164
sungyuha <yuha822@naver.com>
2023-12-08 10:43:31 +0900

    dosc: JS - Fetch API

--- Promise.js
+++ Promise.js
@@ -124,3 +124,23 @@ promise
     .then((data) => {getData()})
     .then((data) => {getData()})
     .then(console.log);
+
+// - Web API 중 
+// Fetch API : 특정 URL로 네트워크 요청을 보내는 비동기 함수, 특정 서버로부터 데이터를 받아오는 일을 할 때 많이 사용
+// Fetch API는 Promise를 return 
+
+// fetch().then().catch().finally() // 후처리를 다양하게 해줄 수 있음
+
+fetch('https://jsonplaceholder.typicode.com/users') // 인자로 URL 집어넣어주면 됨
+    .then((response) => {
+        return response.json(); // response객체 데이터 추출
+    })
+    .then((data) => { // then에서 추출 된 데이터 받아옴
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log('에러가 발생했습니다');
+    })
+    .finally(() => {
+        console.log('마무리 작업');
+    });
