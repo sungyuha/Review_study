bb6a33cfd598b4f2e49c701815f486928060d540
YUHASUNG <yuha822@naver.com>
2023-12-23 21:09:38 +0900

    dosc: JS - Async

--- Async.js
+++ Async.js
@@ -1,17 +1,21 @@
 // 출처 : https://www.youtube.com/watch?v=6-8mbuUC3fk
 // Async : 비동기 함수를 정의할 때 사용하는 키워드
 
-function getData() {
+function networkRequest() { // 네트워크 2초가 걸리는 로직 시나리오 
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('데이터를 받아왔습니다');
+            resolve();
+        }, 2000);
+    });
+}
+
+async function getData() {
+    await networkRequest(); // await를 앞에 작성하면 데이터를 기다렸다가 networkRequest() 출력 후 리턴문 호출
     return '코딩';
 }
 
 const data = getData();
-console.log(data); // 문자열 코딩 출력
+user.then((data) => console.log(data)); // Promise 객체가 콘솔에 출력
 
-
-async function getUser() { // 항상 Promise를 리턴하는 비동기 함수가 됨
-    return '코딩';
-}
-
-const user = getUser();
-console.log(user); // Promise 객체가 콘솔에 출력
+// async -> Promise 반환하는 비동기 함수로 변신한다!
