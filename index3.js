75ed10888d88dad5daebda62448018a1832d4dfa
sungyuha <yuha822@naver.com>
2023-12-27 20:38:19 +0900

    dosc: JS - Async & Await 에러처리

--- index3.js
+++ index3.js
@@ -7,6 +7,8 @@ function networkRequest() {
 }
 
 async function getUser() {
+    // 에러처리
+    throw new Error('에러가 발생했어요!');
     await networkRequest();
     return '코딩';
 }
@@ -18,7 +20,18 @@ async function getTodo() {
 
 // getUser() 와 getTodo() 함수 호출
 async function getData() {
-    const user = await getUser();
+    // const user = await getUser();
+
+    // try catch로 에러처리
+    let user
+    try {
+        user = await getUser();
+    } catch(error) {
+        // 에러가 잡히면
+        console.log(error.message);
+        user = '익명';
+    }
+
     const todo = await getTodo();
     console.log(`${user}님 ${todo}를 하세요`);
 }
