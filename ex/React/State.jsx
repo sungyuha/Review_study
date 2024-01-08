5a94cdda06c2fb643325f1a8138eb40696108470
sungyuha <yuha822@naver.com>
2024-01-08 19:52:20 +0900

    dosc: React - useState

--- ex/React/State.jsx
+++ ex/React/State.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react';
 
+const heavyWork = () => {
+    console.log('매우 무거운 작업!!!');
+    return ['이민수', '강기영'];
+};
+
 function State() {
-    const [names, setNames] = useState(['']);
+    const [names, setNames] = useState(() => {
+        return heavyWork(); // 초기값을 가져올 때 무거운 작업을 해야 한다면 콜백형태로 값을 리턴해줌
+    });
     const [input, setInput] = useState('');
 
     // onChange 해줄 함수
