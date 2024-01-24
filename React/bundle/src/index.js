36a8c0ab133c1fcde6148f68df2816d7ae5c5e59
YUHASUNG <yuha822@naver.com>
2024-01-24 19:37:00 +0900

    dosc: React - Hooks인 useState, useEffect

--- React/bundle/src/index.js
+++ React/bundle/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CommentList from './components/CommentList';
+import Accommodate from './components/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CommentList />
+    <Accommodate />
   </React.StrictMode>
 );
