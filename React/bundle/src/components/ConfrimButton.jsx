bd5a4f47d7d62b8762f768c5a0f2f936a898626c
sungyuha <yuha822@naver.com>
2024-01-28 19:54:51 +0900

    dosc: React - 클릭 이벤트 처리

---
+++ React/bundle/src/components/ConfrimButton.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+function ConfirmButton(props) {
+    const [isConfirmed, setIsConfirmed] = useState(false);
+
+    const handleConfirm = () => {
+        setIsConfirmed((prevIsConfirmed) => !prevIsConfirmed);
+    };
+
+    return (
+        // 버튼을 누르면 '확인됨'으로 변경되며 클릭을 할 수 없게 만듬
+        <button onClick={handleConfirm} disabled={isConfirmed}>
+            {isConfirmed ? '확인됨' : '확인하기'}
+        </button>
+    );
+}
+
+export default ConfirmButton;
