92262411d05373b05ea8438096eef79049d8a3a5
YUHASUNG <yuha822@naver.com>
2024-02-16 08:56:54 +0900

    dosc: JS - Fetch API JSON 데이터 업로드 사용법

--- JS/Fetch-API.js
+++ JS/Fetch-API.js
@@ -27,4 +27,25 @@ postData("https://example.com/answer", { answer: 42 }).then((data) => {
 console.log(data); // JSON 데이터가 `data.json()` 호출에 의해 파싱됨
 });
 
+// JSON 데이터 업로드
+async function postJSON(data) { // fetch()를 사용하면 JSON 인코딩된 데이터를 POST 요청에 포함할 수 있음
+    try {
+    const response = await fetch("https://example.com/profile", {
+        method: "POST", // 또는 'PUT'
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    console.log("성공:", result);
+    } catch (error) {
+    console.error("실패:", error);
+    }
+}
+
+const data = { username: "example" };
+postJSON(data);
+
 // 출처 : mdn
