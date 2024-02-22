af1642782bba78daae31f97bf6e3656e5bae98e4
YUHASUNG <yuha822@naver.com>
2024-02-22 13:20:50 +0900

    dosc: React - useId

--- React/useId.jsx
+++ React/useId.jsx
@@ -1,6 +1,6 @@
 // useId() : react 버젼에 출시 된 react hooks -> 고유한 아이디를 만들기 위해 사용
 
-import { useId } from 'react';
+import { useEffect, useId, useRef } from 'react';
 
 const App = () => {
     return (
@@ -11,16 +11,21 @@ const App = () => {
 }
 
 const MyInput = () => {
-    const id = useId();
-    console.log(id); // 접근성에 유용하게 사용
+    const id = useId(); // 장점 : 안정성
+    const ref = useRef();
+    // console.log(id); // 접근성에 유용하게 사용
+
+    useEffect(() => {
+        const element = ref.current;
+        console.log(element);
+    }, []);
 
     return (
         <div>
             <label htmlFor={`${id}-name`}>이름</label> {/** input마다 고유한 아이디 */}
-            <input id={`${id}-name`} />
-            <br />
-            <label htmlFor={`${id}-age`}>나이</label>
-            <input id={`${id}-age`} />
+            {/* <input id={`${id}-name`} /> */}
+            <input id={id} ref={ref} />
+            <button id='btn'>버튼</button>
         </div>
     );
 }
