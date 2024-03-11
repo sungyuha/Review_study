c97b3287f954ee0f3bb48edca68cc096a8f09f98
YUHASUNG <yuha822@naver.com>
2024-03-11 18:53:11 +0900

    dosc: JS - push method

---
+++ JS/Push.js
@@ -0,0 +1,8 @@
+// 자바스크립트 배열에 원소를 추가하는 방법
+// push() 메서드 : 넘어온 인수는 배열의 끝, 즉 기존에 마지막 원소 바로 뒤에 추가하고 결과로 원소를 추가한 후의 배열에 들어있는 원소의 수, 즉 배열의 길이를 반환
+
+const fruits = ['apple', 'orange', 'coffee'];
+fruits.push('orange'); // 4
+console.log(fruits); // [ 'apple', 'orange', 'coffee', 'orange' ]
+
+// 출처 : https://www.daleseo.com/js-array-push-unshift
