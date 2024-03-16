c1c9dc5456917a3b2aecbc31c5d2c00a9159fc0e
YUHASUNG <yuha822@naver.com>
2024-03-16 15:37:36 +0900

    Dosc: Next - 데이터 조회하기

--- Next/my-app/src/app/read/[id]/page.js
+++ Next/my-app/src/app/read/[id]/page.js
@@ -1,10 +1,13 @@
 // 다이나믹 라우팅
 
-export default function Read(props) {
+export default async function Read(props) { // 서버 컴포넌트로 변경
+    const resp = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+    const topic = await resp.json();
     return (
         <>
             <h2>Read</h2>
-            paramters : {props.params.id} {/** read밑 [id]라는 폴더 안의 컨텐트 값을 가져올 수 있음 */}
+            {topic.body}
+            {/* paramters : {props.params.id} * read밑 [id]라는 폴더 안의 컨텐트 값을 가져올 수 있음 */}
         </>
     )
 }
