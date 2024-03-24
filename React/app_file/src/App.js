d0b6d620464a1cf42127368b41e68fb3abbbf93a
YUHASUNG <yuha822@naver.com>
2024-03-25 00:43:16 +0900

    Dosc: React-hook-form

--- React/app_file/src/App.js
+++ React/app_file/src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <UserContext.Provider value={'사용자'}>
       <ThemeContext.Provider value={{isDark, setIsDark}}> {/** value라는 props를 하나 받음, value안에는 전달하고자 하는 데이터를 넣어주면 됨 */}
-        <Page />
-      </ThemeContext.Provider> /** context를 불러와서 모든 하위 컴포넌트들에게 필요한지 방송 */
+        {/* <Page /> */}
+      </ThemeContext.Provider> {/** context를 불러와서 모든 하위 컴포넌트들에게 필요한지 방송 */}
     </UserContext.Provider>
   );
 }
--- React/app_file/src/index.js
+++ React/app_file/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import CommentList from './components/CommentList';
 import Accommodate from './components/Accommodate';
 import Toolbar from './components/Toolbar';
+import F_hook from './components/F_hook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    {/* <CommentList />
     <Accommodate />
-    <Toolbar />
+    <Toolbar /> */}
+    <F_hook />
   </React.StrictMode>
 );
