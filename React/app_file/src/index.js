b0b4cdb6341c5e2d01eac0105487baa89553f74f
YUHASUNG <yuha822@naver.com>
2024-03-25 00:57:02 +0900

    dosc: 디렉토리 정리

--- React/app_file/src/index.js
+++ React/app_file/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CommentList from './components/CommentList';
-import Accommodate from './components/Accommodate';
-import Toolbar from './components/Toolbar';
+// import CommentList from './components/CommentList';
+// import Accommodate from './components/Accommodate';
+// import Toolbar from './components/Toolbar';
 import F_hook from './components/F_hook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
